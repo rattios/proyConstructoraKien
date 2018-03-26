21e5c0252197f8135bf3020fe9d0797f8be7a592
freddy gabriel ramirez <freddy gabriel ramirez>
2018-03-26 09:33:31 -0400

    api emails

--- constructoraKienAPI/resources/views/emails/contact.blade.php
+++ constructoraKienAPI/resources/views/emails/contact.blade.php
@@ -1,12 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-	<meta charset="UTF-8">
-	<title>Document</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Email</title>
+	<style type="text/css" media="screen">
+		img{
+			margin: auto;
+			display: block;
+		}
+		.content{
+			border: 25px solid #c71717;
+			padding: 50px 30px;
+			width: 60%;
+			background: linear-gradient(rgba(255,255,255,.9), rgba(255,255,255,.9)),url(http://constructorakien.internow.com.mx/assets/img/fondoLogin.png) #fff;
+			background-size: cover;
+		}
+		.title{
+			text-align: center;
+			color: #424242;
+		}
+	</style>
 </head>
 <body>
-	<p><strong>Código de verificación: </strong>{!!$codigo_verificacion!!}</p>
-	<br>
-	<p>Saludos cordiales, el equipo de Constructora Kien.</p>
+	<div class="content">
+		<div class="content-img">
+			<img src="http://constructorakien.internow.com.mx/terms/logotext.png" height="120px" alt="constructorakien">
+		</div>
+		<br>
+		<h3 class="title">RECUPERAR CONSTRASEÑA</h3>
+		<hr><br><br>
+		Su Código de verificación es el siguiente: <strong>{!!$codigo_verificacion!!}</strong>
+		<br><br>
+		<p>Saludos cordiales, equipo de Constructora Kien.</p>
+	</div>
 </body>
 </html>
